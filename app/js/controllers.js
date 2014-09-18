df6b6ced86c3fbc5de3ee26b19da4dba6a8d9cf5
DJ <misurelli2@gmail.com>
2014-09-18 11:06:05 -0600

    changes to show 4 new phones

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,11 +6,13 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function($scope) {
   $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+    {'name': 'Galaxy S5',
+     'snippet': 'dont wory the S6 is coming next month.'},
+    {'name': 'iPhone 6',
+     'snippet': 'kinda looks like the iPod 3.'},
+    {'name': 'LG G3',
+     'snippet': 'Sweet screen.'},
+    {'name': 'Windows phone?',
+     'snippet': 'LOL Yeah right!'}
   ];
 });
